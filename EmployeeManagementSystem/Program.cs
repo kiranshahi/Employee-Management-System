127506455eec3e10eca9ad4f69c74bf717cf980b
Kiran Shahi <kiran.shahi.np@gmail.com>
2023-02-19 19:33:39 +0000

    upgraded to .NET 7.0 Standard Term Support (STS)

--- EmployeeManagementSystem/Program.cs
+++ EmployeeManagementSystem/Program.cs
@@ -12,6 +12,7 @@ namespace EmployeeManagementSystem
         static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new EmployeeSysMainForm());
         }
